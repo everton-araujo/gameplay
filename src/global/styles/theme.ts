ff49acf339c2ac56fc232775d714f4167ea03619
Everton Araujo <everton.aaraujo@outlook.com>
2021-08-08 21:35:20 -0300

    Google fonts added

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,5 +4,11 @@ export const theme = {
     heading: '#DDE3F0',
     primary: '#E51C44',
     line: '#991F36',
+  },
+  fonts: {
+    main: 'Inter-Regular',
+    mainMedium: 'Inter-Medium',
+    second: 'Rajdhani-Medium',
+    secondBold: 'Rajdhani-Bold'
   }
 }
