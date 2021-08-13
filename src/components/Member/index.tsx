54d61b81c18c1aa761f701207873d1e8904b1eaf
Everton Araujo <everton.aaraujo@outlook.com>
2021-08-13 19:31:55 -0300

    AppointmentDetails page created

---
+++ src/components/Member/index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+import { Avatar } from "../Avatar";
+
+import { styles } from './styles';
+
+export type MemberProps = {
+  data: {
+    id: string;
+    username: string;
+    avatar_url: string;
+    status: string;
+  }
+}
+
+export function Member({ data }: MemberProps) {
+  const isOnline = data.status === 'online';
+  const {primary, on} = theme.colors;
+
+  return (
+    <View style={styles.container}>
+      <Avatar urlImage={data.avatar_url} />
+
+      <View>
+        <Text style={styles.title}>
+          { data.username }
+        </Text>
+
+        <View style={styles.status}>
+          <View 
+            style={[
+              styles.bulletStatus,
+              {
+                backgroundColor: isOnline ? on : primary
+              }
+            ]}
+          />
+
+          <Text style={styles.nameStatus}>
+            {
+              isOnline ? 'Disponível' : 'Ocupado'
+            }
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { FlatList, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { Appointment } from '../../components/Appointment';
 import { Background } from '../../components/Background';
@@ -13,6 +14,8 @@ import { styles } from './styles';
 
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const navigation = useNavigation();
 
   const appointments = [
     {
@@ -31,6 +34,10 @@ export function Home() {
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
+  function handleAppointmentDetails() {
+    navigation.navigate('AppointmentDetails');
   }
 
   return (
@@ -55,7 +62,10 @@ export function Home() {
           data={appointments}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <Appointment data={item} />
+            <Appointment 
+              data={item}
+              onPress={handleAppointmentDetails}
+            />
           )}
           showsVerticalScrollIndicator={false}
           ItemSeparatorComponent={() => <ListDivider />}
